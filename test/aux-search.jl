d932683cc10da1f000b29a4a45c0462cb6148d1a
Paul Jensen <software@jensenlab.net>
2021-11-19 05:28:51 -0600

    initial returns

--- test/aux-search.jl
+++ test/aux-search.jl
@@ -172,9 +172,7 @@ function StandardLP(cobra::Tiger.CobraModel)
     )
 end
 
-function find_conditional_media(cobra::Tiger.CobraModel, ko; min_wt_growth=1.0, max_ko_growth=0.0)
-    gene_names = cobra.genes
-
+function find_conditional_media(cobra::Tiger.CobraModel, ko; min_wt_growth=1.0, max_ko_growth=0.0, exchanges=nothing, max_kos=0, optimize=true)
     lp = StandardLP(cobra)
     n = length(lp.vars)
 
@@ -188,72 +186,10 @@ function find_conditional_media(cobra::Tiger.CobraModel, ko; min_wt_growth=1.0, 
     @constraint(model, lp.A * v_wt .<= lp.b)
     @constraint(model, lp.Aeq * v_wt .== lp.beq)
 
-    # add KO condition with primal-dual expansion
-    v_ko = @variable(model, [1:n])
-    set_name.(v_ko, lp.vars .* "_KO")
-    set_lower_bound.(v_ko, lp.lb)
-    set_upper_bound.(v_ko, lp.ub)
-    @constraint(model, lp.A * v_ko .<= lp.b)
-    @constraint(model, lp.Aeq * v_ko .== lp.beq)
-
-    @variable(model, lambda[1:size(lp.Aeq,1)])
-    @variable(model, mu[1:size(lp.A,1)] >= 0)
-    @variable(model, muU[1:n] >= 0)
-    @variable(model, muL[1:n] >= 0)
-
-    @constraint(model, lp.Aeq'*lambda + lp.A'*mu - muL + muU .== lp.sense*lp.c)
-
-    ex_names = get_exchange_rxns(cobra; objective=false)
-    nex = length(ex_names)
-    @variable(model, I[1:nex], binary=true)
-    for (i, ex) in enumerate(ex_names)
-        # bind all variable except the KO variable
-        if ex != ko
-            j = findfirst(ex .== lp.vars)
-            @constraint(model, !I[i] => {v_wt[j] == 0.0})
-            @constraint(model, !I[i] => {v_ko[j] == 0.0})
-
-            # transfer switching constraint to the dual except for
-            # any reaction in the primal's objective function
-            if lp.c[j] == 0
-                @constraint(model, I[i] => {muU[j] == 0.0})
-                @constraint(model, I[i] => {muL[j] == 0.0})
-            end
-        end
-    end
-
-    # strong duality
-    i = [x in ex_names for x in lp.vars]
-    @constraint(model, lambda'*lp.beq + mu'*lp.b - muL[.!i]'*lp.lb[.!i] + muU[.!i]'*lp.ub[.!i] - muL[i]'*(lp.lb[i] .* I) + muU[i]'*(lp.ub[i] .* I) == lp.sense*lp.c'*v_ko)
-
-    # knockout KO variable
-    set_upper_bound(variable_by_name(model, ko * "_KO"), 0.0)
-    set_lower_bound(variable_by_name(model, ko * "_KO"), 0.0)
-
-    # constraint WT to grow
-    @constraint(model, lp.sense*lp.c' * v_wt >= min_wt_growth)
-
-    # constraint KO to not grow
-    @constraint(model, lp.sense*lp.c' * v_ko <= max_ko_growth)
-
-    @objective(model, Min, sum(I))
-
-    return model
-end
-
-function find_conditional_media_ex(cobra::Tiger.CobraModel, ko; min_wt_growth=1.0, max_ko_growth=0.0)
-    lp = StandardLP(cobra)
-    n = length(lp.vars)
-
-    model = Model(Gurobi.Optimizer)
-
-    # add WT condition
-    v_wt = @variable(model, [1:n])
-    set_name.(v_wt, lp.vars .* "_WT")
-    set_lower_bound.(v_wt, lp.lb)
-    set_upper_bound.(v_wt, lp.ub)
-    @constraint(model, lp.A * v_wt .<= lp.b)
-    @constraint(model, lp.Aeq * v_wt .== lp.beq)
+    # find the WT growth
+    @objective(model, Max, cobra.c'*v_wt)
+    optimize!(model)
+    wt_growth = objective_value(model)
 
     # add KO condition with primal-dual expansion
     ko_idx = findfirst(x->x==ko, lp.vars)
@@ -274,7 +210,12 @@ function find_conditional_media_ex(cobra::Tiger.CobraModel, ko; min_wt_growth=1.
     @variable(model, muL[1:n] >= 0)
 
     @variable(model, I[1:n], binary=true)
-    ex_names = get_exchange_rxns(cobra, objective=false)
+
+    if isnothing(exchanges)
+        ex_names = get_exchange_rxns(cobra, objective=false)
+    else
+        ex_names = exchanges
+    end
     Ie = [x in ex_names for x in lp.vars]
 
     gene_names = cobra.genes
@@ -286,173 +227,67 @@ function find_conditional_media_ex(cobra::Tiger.CobraModel, ko; min_wt_growth=1.
 
     @constraint(model, v_wt[Ie] .<= lp.ub[Ie].*I[Ie])
     @constraint(model, v_ko[Ie] .<= ko_ub[Ie].*I[Ie])
-    @constraint(model, v_wt[Ig] .<= lp.ub[Ig].*I[Ig])
-    @constraint(model, v_ko[Ig] .<= ko_ub[Ig].*I[Ig])
+    if max_kos > 0
+        @constraint(model, v_wt[Ig] .<= lp.ub[Ig].*I[Ig])
+        @constraint(model, v_ko[Ig] .<= ko_ub[Ig].*I[Ig])
+        @constraint(model, sum(Ig) - sum(I[Ig]) <= max_kos)
+    end
     #@constraint(model, muI[Ie] .<= M*(1 .- I[Ie]))
 
     # strong duality
-    
-    #@constraint(model, lambda'*lp.beq - muL[.!i]'*lp.lb[.!i] + muU[.!i]'*lp.ub[.!i] - muL[i]'*(lp.lb[i] .* I[i]) + muU[i]'*(lp.ub[i] .* I[i]) == -lp.sense*lp.c'*v_ko)
     @constraint(model, lambda'*lp.beq + mu'*lp.b - muL'*ko_lb + muU'*(ko_ub.*I) == -lp.sense*lp.c'*v_ko)
 
     # constraint WT to grow
-    @constraint(model, lp.c' * v_wt >= min_wt_growth)
+    @constraint(model, lp.c' * v_wt >= min_wt_growth * wt_growth)
 
     # constraint KO to not grow
-    @constraint(model, lp.c' * v_ko <= max_ko_growth)
+    @constraint(model, lp.c' * v_ko <= max_ko_growth * wt_growth)
 
-    @objective(model, Max, sum(I[Ig]) - sum(I[Ie]))
-
-    return model
-end
-
-function find_conditional_media_rxns(cobra::Tiger.CobraModel, ko; min_wt_growth=1.0, max_ko_growth=0.0)
-    # we're assume a non-extended Cobra model, so only Sv=0 constraints
-
-    m, n = size(cobra.S)
-
-    # add WT condition
-    model = Model(Gurobi.Optimizer)
-    v_wt = @variable(model, [1:n])
-    set_name.(v_wt, cobra.rxns .* "_WT")
-    set_lower_bound.(v_wt, cobra.lb)
-    set_upper_bound.(v_wt, cobra.ub)
-    @constraint(model, cobra.S * v_wt .== cobra.b)
-
-    # add KO condition with strong duality
-    v_ko = @variable(model, [1:n])
-    set_name.(v_ko, cobra.rxns .* "_KO")
-    set_lower_bound.(v_ko, cobra.lb)
-    set_upper_bound.(v_ko, cobra.ub)
-    @constraint(model, cobra.S * v_ko .== cobra.b)
-
-    @variable(model, lambda[1:m])
-    @variable(model, muU[1:n] >= 0)
-    @variable(model, muL[1:n] >= 0)
-
-    @constraint(model, cobra.S'*lambda - muL + muU .== cobra.c)
-
-    @variable(model, I[1:n], binary=true)
-    for i = 1:n
-        # bind all variable except the KO variable
-        if name(v_wt[i]) != ko * "_WT"
-            @constraint(model, !I[i] => {v_wt[i] == 0.0})
-            @constraint(model, !I[i] => {v_ko[i] == 0.0})
-        end
+    if max_kos > 0
+        @objective(model, Max, sum(I[Ig]) - sum(I[Ie]))
+    else
+        @objective(model, Min, sum(I[Ie]))
     end
 
-    for i = 1:n
-        # transfer switching constraint to the dual except for
-        # any reaction in the primal's objective function
-        if cobra.c[i] == 0
-            @constraint(model, I[i] => {muU[i] == 0.0})
-            @constraint(model, I[i] => {muL[i] == 0.0})
-        end
+    if !optimize
+        media = nothing
+        kos = nothing
+    else
+        optimize!(model)
+        media = exchanges[value.(I[Ie]) .≈ 1.0]
+        kos = gene_names[value.(I[Ig]) .≈ 0.0]
     end
 
-    # strong duality
-    @constraint(model, lambda'*cobra.b - muL'*(cobra.lb.*I) + muU'*(cobra.ub.*I) == cobra.c'*v_ko)
-
-    # knockout KO variable
-    set_upper_bound(variable_by_name(model, ko * "_KO"), 0.0)
-    set_lower_bound(variable_by_name(model, ko * "_KO"), 0.0)
-
-    # constraint WT to grow
-    @constraint(model, cobra.c' * v_wt >= min_wt_growth)
-
-    # constraint KO to not grow
-    @constraint(model, cobra.c' * v_ko <= max_ko_growth)
-
-    @objective(model, Max, sum(I))
-
-    return model
-
+    return model, media, kos
 end
-
 
 # =================== TESTING ===================
 
 
-cobra = read_cobra("/Users/jensen/Dropbox/research/tiger.jl/Tiger/test/cobra_or.mat", "cobra")
-#cobra = read_cobra("/Users/jensen/Dropbox/research/tiger.jl/Tiger/test/cobra_small.mat", "cobra")
+# cobra = read_cobra("/Users/jensen/Dropbox/research/tiger.jl/Tiger/test/cobra_or.mat", "cobra")
+# #cobra = read_cobra("/Users/jensen/Dropbox/research/tiger.jl/Tiger/test/cobra_small.mat", "cobra")
 
-cobra = extend_cobra_cnf(cobra, ub=1.0)
-model = find_conditional_media_ex(cobra, "g6a", min_wt_growth=0.1)
-optimize!(model)
-
-# cobra.ub[9] = 0.1
-# cobra.ub[[8 10]] .= 0.0
-
-
-# #model = build_base_model(cobra)
-# #optimize!(model)
-# #fitness = single_deletions(model, variable_by_name.(model, cobra.genes))
-
-# # model1 = find_conditional_media_ex(cobra, "r6", min_wt_growth=0.1)
-# # optimize!(model1)
-# # model2 = find_conditional_media_rxns(cobra, "r6", min_wt_growth=0.1)
-# # optimize!(model2)
-
-# ko = "r5"
-# ko_idx = findfirst(x->x==ko, cobra.vars)
-# cobra.lb[ko_idx] = 0.0
-# cobra.ub[ko_idx] = 0.0
-
-# #cobra.lb[1] = 0.0
-# #cobra.ub[1] = 0.0
-
-# lp = StandardLP(cobra)
-# n = length(lp.vars)
-
-# model = Model(Gurobi.Optimizer)
-
-# # add KO condition with primal-dual expansion
-# v_ko = @variable(model, [1:n])
-# set_name.(v_ko, lp.vars .* "_KO")
-# set_lower_bound.(v_ko, lp.lb)
-# set_upper_bound.(v_ko, lp.ub)
-# #@constraint(model, lp.A * v_ko .<= lp.b)
-# @constraint(model, lp.Aeq * v_ko .== lp.beq)
-
-# @variable(model, lambda[1:size(lp.Aeq,1)])
-# #@variable(model, mu[1:size(lp.A,1)] >= 0)
-# @variable(model, muU[1:n] >= 0)
-# @variable(model, muL[1:n] >= 0)
-
-# #@variable(model, I[1:n], binary=true)
-# #@variable(model, muI[1:n] >= 0)
-# #ex_names = get_exchange_rxns(cobra, objective=false)
-# # Ie = [x in ex_names for x in lp.vars]
-# # fix.(I[.!Ie], 1)
-
-# #@constraint(model, lp.Aeq'*lambda + lp.A'*mu - muL + muU .== lp.sense*lp.c)
-# @constraint(model, lp.Aeq'*lambda - muL + muU .== -lp.sense*lp.c)
-
-# @variable(model, I[1:n], binary=true)
-# for i in [1 2 3]
-#     #@constraint(model, v_ko[i] <= I[i])
-#     @constraint(model, !I[i] => {v_ko[i] == 0.0})
-#     #@constraint(model, muU[i] <= 1 - I[i])
-# end
-# @constraint(model, I[4:10] .== 1)
-
-
-# # M = 2
-# # @constraint(model, v_wt[Ie] .<= M*I[Ie])
-# # @constraint(model, v_ko[Ie] .<= M*I[Ie])
-# # @constraint(model, muI[Ie] .<= M*(1 .- I[Ie]))
-
-# # strong duality
-
-# #@constraint(model, lambda'*lp.beq - muL[.!i]'*lp.lb[.!i] + muU[.!i]'*lp.ub[.!i] - muL[i]'*(lp.lb[i] .* I[i]) + muU[i]'*(lp.ub[i] .* I[i]) == -lp.sense*lp.c'*v_ko)
-# #@constraint(model, lambda'*lp.beq - muL'*lp.lb + muU'*lp.ub + muI'*(M*I) == -lp.sense*lp.c'*v_ko)
-# @constraint(model, lambda'*lp.beq - muL'*lp.lb + muU'*(lp.ub.*I) == -lp.sense*lp.c'*v_ko)
-
-# #fix(variable_by_name(model, "r1_KO"), 0, force=true)
-
-# @constraint(model, cobra.c' * v_ko <= 0)
-
-# @objective(model, Max, sum(I))
-
+# cobra = extend_cobra_cnf(cobra, ub=1.0)
+# model = find_conditional_media(cobra, "g6a", min_wt_growth=0.1)
 # optimize!(model)
-# #solution_summary(model, verbose=true)
+
+
+cobra = read_cobra("/Users/jensen/Dropbox/repos/COBRA_models/iSMU.mat", "iSMU")
+cobra.lb[cobra.lb .> 0.0] .= 0.0  # remove NGAM
+cobra = extend_cobra_cnf(cobra, ub=1000)
+
+# we only want the exchanges with positive fluxes; these are
+# the media exchanges in iSMU
+ex_names = get_exchange_rxns(cobra, objective=false)
+is_ex = [x in ex_names for x in cobra.vars]
+ingredients = ex_names[nonzeros(cobra.S[:,is_ex]) .> 0]
+is_ingredient = [x in ingredients for x in cobra.vars]
+
+cobra.ub[is_ingredient] .= 10
+cobra.ub[cobra.ub .>  1000] .=  1000
+cobra.lb[cobra.lb .< -1000] .= -1000
+
+optimize!(build_base_model(StandardLP(cobra)))
+
+model, media, kos = find_conditional_media(cobra, "SMU_308", min_wt_growth=0.3, exchanges=ingredients)
+#optimize!(model)
